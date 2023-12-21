d9d6fc9deeded704662bf659bfcf170422da8082
harsh2799 <mishraharsh27@gmail.com>
2023-12-20 21:34:56 -0500

    Adding code for Mongo

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, Blueprint
 from flask_restful import Api
+from pymongo import MongoClient
 
 money_blueprint = Blueprint('money_blueprint', __name__)
 
 app = Flask(__name__)
 api = Api(app)
+client = MongoClient()
 
 from app import routes
